f155de80d00cdd7543233b647a871dcd97286d64
Matthew Fong <matthewykfong@gmail.com>
2021-10-28 19:29:14 -0400

    loopback

--- weather-client.py
+++ weather-client.py
@@ -2,7 +2,7 @@
 
 import socket
 
-HOST = '192.168.2.3'  # The server's hostname or IP address
+HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 8080        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
--- weather-server.py
+++ weather-server.py
@@ -2,7 +2,7 @@
 
 import socket
 
-HOST = '192.168.2.3'  # Standard loopback interface address (localhost)
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 8080        # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
